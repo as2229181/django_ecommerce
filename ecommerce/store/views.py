43bd849678fd8944d7b9a7f66b8c474da3b9ada7
as2229181 <as2229181@gmail.com>
2023-03-30 22:36:56 +0800

    cart,store,checkout basic page done ,next add fuction like addtotheacrt etc.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,10 +7,34 @@ def store(request):
              'Products':products}
     return render(request,"Store.html",context)
 def cart(request):
-    context={'word':'this is cart page'}
+    customer=None
+    if request.user.is_authenticated: #確認user是有被授權的
+        try:
+            customer= request.user.customer #獲得user作為customer的資料
+        except Customer.DoesNotExist:
+            pass
+        if customer  is not None:
+            order,created=Order.objects.get_or_create(customer=customer,complete=False)#如果user還不是
+            items=order.orderitem_set.all()
+        else:
+            items=[]
+            order={"get_cart_items":0,"get_cart_total":0}
+    context={'items':items,"order":order}
     return render(request,"Cart.html",context)
 def checkout(request):
-    context={'word':'this is chechout page'}
+    customer=None
+    if request.user.is_authenticated: #確認user是有被授權的
+        try:
+            customer= request.user.customer #獲得user作為customer的資料
+        except Customer.DoesNotExist:
+            pass
+        if customer  is not None:
+            order,created=Order.objects.get_or_create(customer=customer,complete=False)#如果user還不是
+            items=order.orderitem_set.all()
+        else:
+            items=[]
+            order={"get_cart_items":0,"get_cart_total":0}
+    context={'items':items,"order":order}
     return render(request,"Checkout.html",context)
 
 
